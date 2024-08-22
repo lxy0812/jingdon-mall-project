e68a42f2c4811a09ebcc2a8a1ecc76d252995b3e
Overlord2002 <3415895622@qq.com>
2024-08-22 20:10:00 +0800

    轮播图

--- js/index.js
+++ js/index.js
@@ -18,6 +18,8 @@ let searchHelper = document.querySelector('.search-help'); // 获取下拉列表
 // 定义数组，存储搜索内容
 let searchArr = ['华为手机', '苹果手机', '小米手机', '电视机', '笔记本'];
 // 给输入框绑定内容改变事件
+// 添加一个定时器
+let timerInput = null;
 keyword.oninput = function(){
     searchHelper.innerHTML = '';
     for(let i = 0; i < searchArr.length; i++){
@@ -36,4 +38,78 @@ keyword.oninput = function(){
     if (searchHelper.children.length > 0) {
         searchHelper.style.display = 'block'; // 如果有搜索建议，显示下拉列表
     }
+    keyword.onblur = function(){
+        timerInput = setTimeout(() => {
+            searchHelper.style.display = 'none';
+        },500);
+    }
 };
+// 实现轮播图切换
+let img = document.querySelector('.img');
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+let fs_col2 = document.querySelector('.fs_col2');
+let lis = document.querySelectorAll('.banner-btn li');
+
+let imgArr = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
+let count = 0;
+// 定义函数，用来切换图片路径
+function cutImg(){
+    img.src = './img/轮播图/' + imgArr[count];
+    for(let i = 0; i < lis.length; i++){
+        lis[i].className = '';
+    }
+    lis[count].className = 'active';
+}
+//设置定时器，每隔3秒切换图片
+let timer = setInterval(function(){
+    count++;
+    if(count > imgArr.length - 1){
+        count = 0;
+    }
+    cutImg();
+},3000);
+// 点击上一张
+prev.onclick = function(){
+    count--;
+    if(count < 0){
+        count = imgArr.length - 1;
+    }
+    cutImg();
+}
+// 点击下一张
+next.onclick = function(){
+    count++;
+    if(count > imgArr.length - 1){
+        count = 0;
+    }
+    cutImg();
+}
+// 鼠标划入，关掉定时切换图片
+fs_col2.onmouseover = function(){
+    clearInterval(timer);
+}
+// 鼠标划出，启动定时切换图片
+fs_col2.onmouseout = function(){
+    timer = setInterval(function(){
+        count++;
+        if(count > imgArr.length - 1){
+            count = 0;
+        }
+        cutImg();
+    },3000);
+}
+/* // 给li绑定点击事件
+for (let i = 0; i < lis.length; i++){
+    lis[i].onclick = () => {
+        count = i;
+        cutImg();
+    };
+} */
+// 鼠标落下触发
+for (let i = 0; i < lis.length; i++){
+    lis[i].onmouseover = () => {
+        count = i;
+        cutImg();
+    };
+} 
